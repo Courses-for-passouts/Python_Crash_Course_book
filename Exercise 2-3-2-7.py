482f20f20277db77603bb07e40950f49c05c620c
shah nawaz <96956049+shahnawaz323@users.noreply.github.com>
2023-01-14 18:16:31 +0100

    Exercise 2-7 to 2-9

---
+++ Exercise 2-3-2-7.py
@@ -0,0 +1,67 @@
+# Exercise 2-3
+
+name = "Shah"
+message = name+"Salam"
+
+# Exercise 2-4
+print(name.upper())
+print(name.lower())
+
+print(name.title())
+
+# Exercise 2-5
+
+qoute = 'Albert Einstein once said , "A person who never made amistake never tried anything new."'
+
+print(qoute)
+
+# Exercise 2-6
+
+famous_person = "Albert Einstein"
+message = "A person who never made amistake never tried anything new."
+
+print(famous_person +' ' +message)
+
+print(f"{famous_person} {message}")
+
+# Exercise 2-7
+
+name = ' Shah '
+name.lstrip()
+
+name.rstrip()
+
+name.strip()
+
+name = 'Shah'
+
+new_string = '''Hi this a normal string with f 
+new line.'''
+print(new_string)
+print(f"{message},\n\t{name}")
+
+print(f"Hi this a normal string with f \n new line")
+
+print(f"Hi this a normal string with f new line")
+
+print(f"{message},{name}")
+
+number = 1_4
+number
+
+# Exercise 2-8
+
+print(5+3)
+print(10-2)
+print(2*4)
+print(16/2)
+print(2**3)
+
+# Exercise 2-9
+
+favourite_number = 9
+# these two method are valid and equal 
+print("My favt number ",favourite_number)
+print(f"My favt number {favourite_number}")
+
+
