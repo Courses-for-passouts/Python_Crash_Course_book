ee5c8bb7447357f638118c7c0dcc710756c00fb6
Taqi Haider <taqihaider592@gmail.com>
2023-02-21 09:18:55 +0000

    other codes added

---
+++ day2.py
@@ -0,0 +1,12 @@
+name = ' Haider '
+# print(f"\t{name}")
+print(name)
+print(name.strip())
+
+
+
+print(4+4, 14-6, 4*2, 16/2)
+fav_num= 14
+print("My favourite Number is " +  str(fav_num))
+
+print(f"My favourite Number is {fav_num}")
